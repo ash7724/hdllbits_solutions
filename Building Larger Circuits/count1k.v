6e1e3244ebc0a3751354aa3feb42c12bb4cf2410
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-05-02 13:00:40 +0530

    Update count1k.v

--- Building Larger Circuits/count1k.v
+++ Building Larger Circuits/count1k.v
@@ -16,3 +16,21 @@ module top_module #(parameter count_val=999)(
   end
   assign q= counter;
 endmodule
+
+//Build a counter that counts from 0 to 999, inclusive, with a period of 1000 cycles. The reset input is synchronous, and should reset the counter to 0.
+//Second approach segrregating combo and sequential circuit
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q);
+   
+    reg [9:0] q_reg,q_next;
+    always @(posedge clk) begin
+        if(reset)
+            q_reg<='h0;
+        else
+            q_reg<=q_next;
+    end
+    assign q_next=(q_reg==10'h3E7) ? 10'h0 : q_reg+1'b1;
+    assign q=q_reg;
+endmodule
